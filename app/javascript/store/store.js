d5bdcbe123efb9df3f6e70446a9cbc03911fd86e
Saheed <sdrfngg640@gmail.com>
2019-11-27 16:06:11 +0100

    Import thunk

--- app/javascript/store/store.js
+++ app/javascript/store/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 import reducer from '../reducers';
 
 const initialState = {
@@ -35,6 +35,6 @@ const initialState = {
   ],
 };
 
-const store = createStore(reducer, initialState);
+const store = createStore(reducer, initialState, applyMiddleware(thunk));
 
 export default store;
