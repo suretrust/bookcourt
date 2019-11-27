71d360429b0f2779bc1f1db278b179a71b2c2707
Saheed <sdrfngg640@gmail.com>
2019-11-27 18:02:27 +0100

    Update routes to only pass html requests to rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
+  namespace :api, defaults: { format: 'json' } do
+    namespace :v1, defaults: { format: 'json' } do
       get 'courts', to: 'courts#index'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # get '*page', to: 'static#index', constraints: lambda { |req|
-  #   !req.xhr? && req.format.html?
-  # }
+  get '*page', to: 'static#index', constraints: lambda { |req|
+    !req.xhr? && req.format.html?
+  }
   root 'static#index'
 end
