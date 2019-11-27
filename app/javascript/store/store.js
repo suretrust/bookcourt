919ca63225b350d6ac57c4d404dc8ce898e1bbb8
Saheed <sdrfngg640@gmail.com>
2019-11-27 18:02:07 +0100

    Remove initaial state data

--- app/javascript/store/store.js
+++ app/javascript/store/store.js
@@ -3,36 +3,7 @@ import thunk from 'redux-thunk';
 import reducer from '../reducers';
 
 const initialState = {
-  courts: [
-    {
-      name: 'First Court',
-      location: 'Kano, Nigeria',
-      cost: 5000,
-      courtType: 'Carpet Courts',
-      courtDetails: 'The first tennis court in Nigeria.',
-    },
-    {
-      name: 'Second Court',
-      location: 'Kastina, Nigeria',
-      cost: 6000,
-      courtType: 'Clay Courts',
-      courtDetails: 'The second tennis court in Nigeria.',
-    },
-    {
-      name: 'Third Court',
-      location: 'Kaduna, Nigeria',
-      cost: 7000,
-      courtType: 'Hard Courts',
-      courtDetails: 'The third tennis court in Nigeria.',
-    },
-    {
-      name: 'Fourth Court',
-      location: 'Abuja, Nigeria',
-      cost: 8000,
-      courtType: 'Grass Courts',
-      courtDetails: 'The fourth tennis court in Nigeria.',
-    },
-  ],
+  courts: [],
 };
 
 const store = createStore(reducer, initialState, applyMiddleware(thunk));
