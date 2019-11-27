e51f1842851ddf850bde25f2fcd09ffef9234bef
Saheed <sdrfngg640@gmail.com>
2019-11-27 16:05:10 +0100

    Update json response from controller

--- app/controllers/api/v1/courts_controller.rb
+++ app/controllers/api/v1/courts_controller.rb
@@ -1,12 +1,16 @@
+# frozen_string_literal: true
+
 class Api::V1::CourtsController < ApplicationController
   def index
-    render json: { :courts => [
-      {
-        name: 'some-thing',
-        location: 'Nigeria',
-        cost: 5000,
-        court_type: 'Carpet'
-      }
-    ]}.to_json
+    respond_to do |format|
+      format.json do
+        render json: {
+          name: 'some-thing',
+          location: 'Nigeria',
+          cost: 5000,
+          court_type: 'Carpet'
+        }
+      end
+    end
   end
 end
