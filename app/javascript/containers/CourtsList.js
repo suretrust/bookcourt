f81d0c80c600c81e9a579d84ba917ec39b8e176c
Saheed <sdrfngg640@gmail.com>
2019-11-27 18:02:55 +0100

    Add api call to courts table

--- app/javascript/containers/CourtsList.js
+++ app/javascript/containers/CourtsList.js
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import axios from 'axios';
 import Court from '../components/Court';
 import CourtTypes from '../components/CourtTypes';
-import store from '../store/store';
 
 const mapStateToProps = state => ({
   courts: state.courts,
 });
 
-const CourtsList = ({ courts }) => {
-  console.log(store.getState());
+const CourtsList = () => {
+  const [courts, setCourts] = useState([]);
+  const [error, setError] = useState('');
+
   const getCourts = async () => {
-    const data = await fetch('api/v1/courts').then(r => r.json());
-    console.log(data);
-    return data;
+    await axios
+      .get('api/v1/courts')
+      .then(res => {
+        console.log(res.data);
+        setCourts(res.data);
+      })
+      .catch(err => {
+        setError(err);
+      });
   };
 
-  getCourts();
+  useEffect(() => {
+    getCourts();
+  }, []);
 
   return (
     <section>
@@ -25,24 +34,13 @@ const CourtsList = ({ courts }) => {
       <h3>Filter by: </h3>
       <CourtTypes />
       <div>
+        {error ? `${error}` : ''}
         {courts.map(court => (
-          <Court key={court.name} court={court} />
+          <Court key={court.id} court={court} />
         ))}
       </div>
     </section>
   );
-};
-
-CourtsList.propTypes = {
-  courts: PropTypes.arrayOf(
-    PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      location: PropTypes.string.isRequired,
-      cost: PropTypes.number.isRequired,
-      courtType: PropTypes.string.isRequired,
-      courtDetails: PropTypes.string.isRequired,
-    })
-  ).isRequired,
 };
 
 export default connect(mapStateToProps, null)(CourtsList);
