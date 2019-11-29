8b195f706098c6646ae4d872b8f494606cd3834c
Saheed <sdrfngg640@gmail.com>
2019-11-29 21:27:22 +0100

    Remove redundant api call

--- app/javascript/containers/CourtsList.js
+++ app/javascript/containers/CourtsList.js
@@ -1,38 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
-import axios from 'axios';
 import Court from '../components/Court';
-import CourtTypes from '../components/CourtTypes';
 
 const mapStateToProps = state => ({
   courts: state.courts,
 });
 
-const CourtsList = () => {
-  const [courts, setCourts] = useState([]);
-  const [error, setError] = useState('');
-
-  const getCourts = async () => {
-    await axios
-      .get('api/v1/courts')
-      .then(res => {
-        setCourts(res.data);
-      })
-      .catch(err => {
-        setError(err);
-      });
-  };
-
-  useEffect(() => {
-    getCourts();
-  }, []);
-
+const CourtsList = ({ courts }) => {
   return (
     <section>
       <input type="text" placeholder="Search for courts" />
-      <CourtTypes />
       <div>
-        {error ? `${error}` : ''}
         {courts.map(court => (
           <Court key={court.id} court={court} />
         ))}
