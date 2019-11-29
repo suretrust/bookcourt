3094ece61751d11b690d397cae7f2a2b52bab410
Saheed <sdrfngg640@gmail.com>
2019-11-29 21:26:49 +0100

    Handle click

--- app/javascript/components/CourtTypes.js
+++ app/javascript/components/CourtTypes.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { COURT_TYPES } from '../utilities/index';
+import camelCase from '../utilities/camelCase';
+import urlCase from '../utilities/urlCase';
 
-const CourtTypes = () => {
-  return (
-    <section>
-      <h2>Choose your prefered court type</h2>
-      {COURT_TYPES.map(courtType => (
-        <Link key={courtType} to="/court-types">
-          {courtType}
-        </Link>
-      ))}
-    </section>
-  );
-};
+class CourtTypes extends React.Component {
+  handleClick = e => {
+    const { value } = e.target;
+  };
+
+  render() {
+    const { handleClick } = this;
+
+    return (
+      <section>
+        <h2>Choose your prefered court type</h2>
+        {COURT_TYPES.map(courtType => (
+          <Link
+            onClick={handleClick}
+            value={camelCase(courtType)}
+            key={courtType}
+            to={`/court-types/${urlCase(courtType)}`}
+          >
+            {courtType}
+          </Link>
+        ))}
+      </section>
+    );
+  }
+}
 
 export default CourtTypes;
