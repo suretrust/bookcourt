9c995060d90e2f426959bbe2e4389cb5267a44a9
Saheed <sdrfngg640@gmail.com>
2019-11-29 21:47:13 +0100

    Implement go back feature on page not found

--- app/javascript/components/NotFound.js
+++ app/javascript/components/NotFound.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const NotFound = () => {
+const NotFound = ({ history }) => {
+  const goBack = () => {
+    history.goBack();
+  };
+
+  const handleClick = () => {
+    goBack();
+  };
+
   return (
     <div>
       <h3>Whoops! Page not found</h3>
+      <button onClick={handleClick}>Go Back</button>
       <Link to="/">Return to Homepage</Link>
     </div>
   );
