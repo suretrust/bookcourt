92a0ef81ab5e0d8da29a464545127b7edb84044a
Saheed <sdrfngg640@gmail.com>
2019-11-29 06:05:44 +0100

    Add header

--- app/javascript/components/CourtTypes.js
+++ app/javascript/components/CourtTypes.js
@@ -5,6 +5,7 @@ import { COURT_TYPES } from '../utilities/index';
 const CourtTypes = () => {
   return (
     <section>
+      <h2>Choose your prefered court type</h2>
       {COURT_TYPES.map(courtType => (
         <Link key={courtType} to="/court-types">
           {courtType}
