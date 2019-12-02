996fe45f5e3b11a5c7797cddb92d8d67a08aeb13
Saheed <sdrfngg640@gmail.com>
2019-12-02 20:03:59 +0100

    Add court data to seed DB

---
+++ db/courts.rb
@@ -0,0 +1,402 @@
+$courts = [
+  {
+    "name": 'Wedgewood Gardens',
+    "location": 'Wedgewood Circle, New York',
+    "court_type": 'Clay Courts',
+    "cost": 2000,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/1619860/pexels-photo-1619860.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Frick Park Clay Court Tennis Club',
+    "location": 'South Braddock Avenue, Pittsburgh',
+    "court_type": 'Clay Courts',
+    "cost": 2000,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/209977/pexels-photo-209977.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Walden Lakewood',
+    "location": 'East Alexander Street, Tampa-st. Petersburg-Clearwater',
+    "court_type": 'Clay Courts',
+    "cost": 2000,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/171568/pexels-photo-171568.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Bellaria',
+    "location": 'South Ocean Boulevard, west palm beach - boca raton',
+    "court_type": 'Clay Courts',
+    "cost": 2000,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/342361/pexels-photo-342361.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'The Woods',
+    "location": 'Spanish Moss Drive, jacksonville',
+    "court_type": 'Clay Courts',
+    "cost": 2000,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/1277397/pexels-photo-1277397.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Delray Swim & Tennis Club',
+    "location": 'Jaeger Drive, west palm beach-boca raton',
+    "court_type": 'Clay Courts',
+    "cost": 2000,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/342359/pexels-photo-342359.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Memorial Field',
+    "location": 'Croyden Avenue, New York',
+    "court_type": 'Clay Courts',
+    "cost": 2000,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/1598347/pexels-photo-1598347.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Herrick Park',
+    "location": 'Main Street, New York',
+    "court_type": 'Clay Courts',
+    "cost": 2000,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/341003/pexels-photo-341003.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Childrens Park',
+    "location": 'River Road, West Palm Beach - Boca Raton',
+    "court_type": 'Clay Courts',
+    "cost": 2000,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/2440324/pexels-photo-2440324.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Martins Landing - River Club',
+    "location": 'Roswell Riverwalk Trail, Atlanta',
+    "court_type": 'Clay Courts',
+    "cost": 2000,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/2403408/pexels-photo-2403408.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Coral Bay',
+    "location": 'North Bay Drive, miami-fort lauderdale',
+    "court_type": 'Grass Courts',
+    "cost": 2000,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/2855556/pexels-photo-2855556.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Coral Bay',
+    "location": 'East Columbus Road, Dayton-Springfield',
+    "court_type": 'Grass Courts',
+    "cost": 2000,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/2694942/pexels-photo-2694942.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Community Park & Pool',
+    "location": 'Desert Willow Drive, Phoenix',
+    "court_type": 'Grass Courts',
+    "cost": 2000,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/1266983/pexels-photo-1266983.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Desert Highlands',
+    "location": 'Spencer Creek Trail, St. Louis',
+    "court_type": 'Grass Courts',
+    "cost": 2000,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/1432034/pexels-photo-1432034.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Spencer Creek Park',
+    "location": 'South Station Avenue, New York',
+    "court_type": 'Grass Courts',
+    "cost": 2000,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/342360/pexels-photo-342360.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'South Station Avenue Tennis Courts',
+    "location": 'Crandon Boulevard, Miami-Fort Lauderdale',
+    "court_type": 'Grass Courts',
+    "cost": 2000,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/2062080/pexels-photo-2062080.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Crandon Tennis Center',
+    "location": 'Lane Northwest, Minneapolis-st. Paul',
+    "court_type": 'Grass Courts',
+    "cost": 2000,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/1662176/pexels-photo-1662176.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'USTA Billie Jean King National Tennis Center',
+    "location": 'Perimeter Road, New York',
+    "court_type": 'Grass Courts',
+    "cost": 2000,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/2694944/pexels-photo-2694944.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Dont care',
+    "location": 'Church Road, Philadelphia',
+    "court_type": 'Grass Courts',
+    "cost": 2000,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/2568551/pexels-photo-2568551.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Snowbird Phase 3 Condominiums',
+    "location": 'Wright Street, Denver',
+    "court_type": 'Grass Courts',
+    "cost": 2000,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/2435292/pexels-photo-2435292.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'University of Michigan Varsity Tennis Center',
+    "location": 'South State Street, Detroit',
+    "court_type": 'Indoor Courts',
+    "cost": 1800,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/1782154/pexels-photo-1782154.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Mellon Park',
+    "location": 'Fifth Avenue, Pittsburgh',
+    "court_type": 'Indoor Courts',
+    "cost": 1800,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/2961946/pexels-photo-2961946.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Whitehaven Tennis Center',
+    "location": 'Finley Road, Memphis',
+    "court_type": 'Indoor Courts',
+    "cost": 1800,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/2961964/pexels-photo-2961964.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Simkins Indoor Tennis Center',
+    "location": 'Dans Road, Greensboro-Winston-Salem',
+    "court_type": 'Indoor Courts',
+    "cost": 1800,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/2629143/pexels-photo-2629143.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Robinswood Tennis Center',
+    "location": 'Avenue Southeast, Seattle',
+    "court_type": 'Indoor Courts',
+    "cost": 1800,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/2815207/pexels-photo-2815207.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Rock Creek Park Tennis Center',
+    "location": 'Stage Road, Washington',
+    "court_type": 'Indoor Courts',
+    "cost": 1800,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/2273/sport-tennis-old-net.jpg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Mt. Lebanon Tennis Center',
+    "location": 'Cedar Boulevard, Pittsburgh',
+    "court_type": 'Indoor Courts',
+    "cost": 1800,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/917503/pexels-photo-917503.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Alley Pond Park',
+    "location": 'Alley Pond Main Path, New York',
+    "court_type": 'Indoor Courts',
+    "cost": 1800,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/1432039/pexels-photo-1432039.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Humboldt Park',
+    "location": 'South Pine Avenue, Milwaukee',
+    "court_type": 'Indoor Courts',
+    "cost": 1800,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/1103829/pexels-photo-1103829.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Bensonhurst Park',
+    "location": 'Bay Parkway, New York',
+    "court_type": 'Indoor Courts',
+    "cost": 1800,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/207361/pexels-photo-207361.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Northwestern High',
+    "location": 'Adelphi Road, Washington',
+    "court_type": 'Hard Courts',
+    "cost": 1400,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/2440324/pexels-photo-2440324.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'El Dorado High School',
+    "location": 'Acacia Way, Sacramento-Yolo',
+    "court_type": 'Hard Courts',
+    "cost": 1400,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/226585/pexels-photo-226585.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Mt. Carmel High School',
+    "location": 'Carmel Mountain Road, San Diego',
+    "court_type": 'Hard Courts',
+    "cost": 1400,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/2973104/pexels-photo-2973104.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Second Street Park',
+    "location": 'South Street, Philadelphia',
+    "court_type": 'Hard Courts',
+    "cost": 1400,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/2121664/pexels-photo-2121664.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Sycamore High School',
+    "location": 'West Street, Chicago',
+    "court_type": 'Hard Courts',
+    "cost": 1400,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/1432039/pexels-photo-1432039.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Mill Pond Park',
+    "location": 'Broadway, Detroit',
+    "court_type": 'Hard Courts',
+    "cost": 1400,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/2403408/pexels-photo-2403408.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Lincoln Park',
+    "location": 'Delaware Avenue, Albany',
+    "court_type": 'Hard Courts',
+    "cost": 1400,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/1671218/pexels-photo-1671218.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Las Vistas At Doral',
+    "location": 'Lake Drive, Miami-fort Lauderdale',
+    "court_type": 'Hard Courts',
+    "cost": 1400,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/1103829/pexels-photo-1103829.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Bellaire',
+    "location": 'Park Street, Houston-Galveston-Brazoria',
+    "court_type": 'Hard Courts',
+    "cost": 1400,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/976873/pexels-photo-976873.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'G.W. Brackenridge High School',
+    "location": 'Eagleland Drive, San Antonio',
+    "court_type": 'Hard Courts',
+    "cost": 1400,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/226563/pexels-photo-226563.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'The Park at Canterbury',
+    "location": 'Brandywood Circle, Orlando',
+    "court_type": 'Carpet Courts',
+    "cost": 2100,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/793176/pexels-photo-793176.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Glenleigh',
+    "location": 'Glenleigh Drive Southwest, Atlanta',
+    "court_type": 'Carpet Courts',
+    "cost": 2100,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/187329/pexels-photo-187329.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Palm Trace Landings',
+    "location": 'Palm Trace Court, Miami-fort Lauderdale',
+    "court_type": 'Carpet Courts',
+    "cost": 2100,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/226562/pexels-photo-226562.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Monterey Village',
+    "location": 'Center Avenue, Los Angeles',
+    "court_type": 'Carpet Courts',
+    "cost": 2100,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/784538/pexels-photo-784538.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Mission Park',
+    "location": 'Woodland Parkway, San Diego',
+    "court_type": 'Carpet Courts',
+    "cost": 2100,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/569986/pexels-photo-569986.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Syd Kronenthal Park',
+    "location": 'Roberts Avenue, Los Angeles',
+    "court_type": 'Carpet Courts',
+    "cost": 2100,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/7087/office-relax-clean-ping-pong.jpg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Trabuco Hills High School',
+    "location": 'Santa Margarita Parkway, Los Angeles',
+    "court_type": 'Carpet Courts',
+    "cost": 2100,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/2299573/pexels-photo-2299573.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'Desert Willows',
+    "location": 'Acoma Avenue, Los Angeles',
+    "court_type": 'Carpet Courts',
+    "cost": 2100,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/226585/pexels-photo-226585.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'St Andrews at Weston',
+    "location": 'Bonaventure Boulevard, Miami-fort Lauderdale',
+    "court_type": 'Carpet Courts',
+    "cost": 2100,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/2568551/pexels-photo-2568551.jpeg?auto=compress&cs=tinysrgb&w=640'
+  },
+  {
+    "name": 'West Memorial Junior High School',
+    "location": 'Merrymount Drive, Houston-Galveston-Brazoria',
+    "court_type": 'Carpet Courts',
+    "cost": 2100,
+    "phone": '(012)3456789',
+    "image": 'https://images.pexels.com/photos/1103833/pexels-photo-1103833.jpeg?auto=compress&cs=tinysrgb&w=640'
+  }
+]
