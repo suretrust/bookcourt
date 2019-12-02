3df6cfde6343278220e48689a9dfe2b65d118390
Saheed <sdrfngg640@gmail.com>
2019-12-02 08:35:55 +0100

    Add prop type validation

--- app/javascript/containers/CourtsList.js
+++ app/javascript/containers/CourtsList.js
@@ -1,22 +1,75 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Court from '../components/Court';
 
 const mapStateToProps = state => ({
   courts: state.courts,
+  carpetCourts: state.carpetCourts,
+  clayCourts: state.clayCourts,
+  indoorCourts: state.indoorCourts,
+  grassCourts: state.grassCourts,
+  hardCourts: state.hardCourts,
 });
 
-const CourtsList = ({ courts }) => {
+const CourtsList = ({
+  match,
+  courts,
+  clayCourts,
+  carpetCourts,
+  grassCourts,
+  hardCourts,
+  indoorCourts,
+}) => {
+  const courtFilter = () => {
+    switch (match.params.type) {
+      case 'all-courts':
+        return courts;
+
+      case 'clay-courts':
+        return clayCourts;
+
+      case 'hard-courts':
+        return hardCourts;
+
+      case 'carpet-courts':
+        return carpetCourts;
+
+      case 'grass-courts':
+        return grassCourts;
+
+      case 'indoor-courts':
+        return indoorCourts;
+
+      default:
+        return courts;
+    }
+  };
+
   return (
     <section>
       <input type="text" placeholder="Search for courts" />
       <div>
-        {courts.map(court => (
+        {courtFilter().map(court => (
           <Court key={court.id} court={court} />
         ))}
       </div>
     </section>
   );
+};
+
+CourtsList.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.objectOf({
+      type: PropTypes.string.isRequired,
+    }),
+  }),
+  courts: PropTypes.arrayOf(PropTypes.object).isRequired,
+  clayCourts: PropTypes.arrayOf(PropTypes.object).isRequired,
+  carpetCourts: PropTypes.arrayOf(PropTypes.object).isRequired,
+  grassCourts: PropTypes.arrayOf(PropTypes.object).isRequired,
+  hardCourts: PropTypes.arrayOf(PropTypes.object).isRequired,
+  indoorCourts: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default connect(mapStateToProps, null)(CourtsList);
