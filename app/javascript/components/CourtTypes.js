2d7b7f5c8f598e307e4379da17180ba8082ba0e8
Saheed <sdrfngg640@gmail.com>
2019-12-02 08:34:28 +0100

    Change to functional component

--- app/javascript/components/CourtTypes.js
+++ app/javascript/components/CourtTypes.js
@@ -1,33 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { COURT_TYPES } from '../utilities/index';
-import camelCase from '../utilities/camelCase';
 import urlCase from '../utilities/urlCase';
 
-class CourtTypes extends React.Component {
-  handleClick = e => {
-    const { value } = e.target;
-  };
-
-  render() {
-    const { handleClick } = this;
-
-    return (
-      <section>
-        <h2>Choose your prefered court type</h2>
-        {COURT_TYPES.map(courtType => (
-          <Link
-            onClick={handleClick}
-            value={camelCase(courtType)}
-            key={courtType}
-            to={`/court-types/${urlCase(courtType)}`}
-          >
-            {courtType}
-          </Link>
-        ))}
-      </section>
-    );
-  }
-}
+const CourtTypes = () => {
+  return (
+    <section>
+      <input type="text" placeholder="Search for a court" />
+      <h2>Choose your prefered court type</h2>
+      {COURT_TYPES.map(courtType => (
+        <Link key={courtType} to={`/court-types/${urlCase(courtType)}`}>
+          {courtType}
+        </Link>
+      ))}
+    </section>
+  );
+};
 
 export default CourtTypes;
