bf1dfc32c601337197f826cff75850b9b2cbc3c1
Saheed <sdrfngg640@gmail.com>
2019-12-05 19:32:58 +0100

    Update footer position in booking confirmed

--- app/javascript/components/BookingConfirmed.js
+++ app/javascript/components/BookingConfirmed.js
@@ -4,18 +4,17 @@ import MobileFooter from './MobileFooter';
 
 const BookingConfirmed = () => {
   return (
-    <section
-      style={{ backgroundColor: '#e2f0d3' }}
-      className="d-flex flex-column text-center align-items-center full-height p-5"
-    >
-      <h2 className="green bold pt-5">Booking Confirmed!</h2>
-      <div className="my-5">
-        <Link to="/court-types" className="btn btn-outline-success mr-2 my-1">
-          Book Another Court
-        </Link>
-        <Link to="/bookings" className="btn btn-success my-1">
-          Check Bookings
-        </Link>
+    <section style={{ backgroundColor: '#e2f0d3' }}>
+      <div className="d-flex flex-column text-center align-items-center full-height p-5">
+        <h2 className="green bold pt-5">Booking Confirmed!</h2>
+        <div className="my-5">
+          <Link to="/court-types" className="btn btn-outline-success mr-2 my-1">
+            Book Another Court
+          </Link>
+          <Link to="/bookings" className="btn btn-success my-1">
+            Check Bookings
+          </Link>
+        </div>
       </div>
       <MobileFooter />
     </section>
