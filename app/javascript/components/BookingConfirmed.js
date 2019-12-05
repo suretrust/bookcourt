fa3c73b176b316479e79e8e55618e10d18c2e0be
Saheed <sdrfngg640@gmail.com>
2019-12-05 10:29:29 +0100

    Update booking confirmed stylings

--- app/javascript/components/BookingConfirmed.js
+++ app/javascript/components/BookingConfirmed.js
@@ -9,18 +9,14 @@ const BookingConfirmed = () => {
       className="d-flex flex-column text-center align-items-center full-height p-5"
     >
       <h2 className="green bold pt-5">Booking Confirmed!</h2>
-      <Row className="p-5 text-center">
-        <Col lg={12} className="my-3">
-          <Link to="/court-types" className="type-btn">
-            Book Another Court
-          </Link>
-        </Col>
-        <Col lg={12} className="my-3">
-          <Link to="/bookings" className="type-btn">
-            Check Bookings
-          </Link>
-        </Col>
-      </Row>
+      <div className="my-5">
+        <Link to="/court-types" className="btn btn-outline-success mr-2">
+          Book Another Court
+        </Link>
+        <Link to="/bookings" className="btn btn-success">
+          Check Bookings
+        </Link>
+      </div>
     </section>
   );
 };
